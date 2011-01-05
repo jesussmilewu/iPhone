f35022dc50f15f2d424bd2b29bbec3d3be9b7bd4
KMR <klaus@rodewig.de>
2011-01-05 22:24:21 +0100

    - Klasse Fahrzeug angelegt - Fahrzeug loggt seine Methodennamen

---
+++ Apps/First Steps/Source/Fahrzeug.h
@@ -0,0 +1,17 @@
+//
+//  Fahrzeug.h
+//  First Steps
+//
+//  Created by Klaus M. Rodewig on 05.01.11.
+//  Copyright 2011 Klaus M. Rodewig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Fahrzeug : NSObject {
+@private
+    
+}
+
+@end
---
+++ Apps/First Steps/Source/Fahrzeug.m
@@ -0,0 +1,28 @@
+//
+//  Fahrzeug.m
+//  First Steps
+//
+//  Created by Klaus M. Rodewig on 05.01.11.
+//  Copyright 2011 Klaus M. Rodewig. All rights reserved.
+//
+
+#import "Fahrzeug.h"
+
+
+@implementation Fahrzeug
+
+- (id)init {
+    if ((self = [super init])) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc {
+    // Clean-up code here.
+    
+    [super dealloc];
+}
+
+@end
