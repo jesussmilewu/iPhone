22ea13add354d3d00bc8ee3ec14733a0d8a47574
KMR <klaus@rodewig.de>
2011-01-05 20:58:13 +0100

    - "First Steps" angelegt

---
+++ Apps/First Steps/Source/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  First Steps
+//
+//  Created by Klaus M. Rodewig on 04.01.11.
+//  Copyright 2011 Klaus M. Rodewig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    // insert code here...
+    NSLog(@"Hello, World!");
+    [pool drain];
+    return 0;
+}
+
