5af295423aa0705bb1b56c7b51eba5599dae9305
Clemens Wagner <clemens@nostromo.local>
2011-01-28 13:04:49 +0100

    Projekt Rotation neu dazu

---
+++ Apps/Rotation/Rotation/RotationViewController.h
@@ -0,0 +1,16 @@
+//
+//  RotationViewController.h
+//  Rotation
+//
+//  Created by Clemens Wagner on 28.01.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationViewController : UIViewController {
+@private
+    
+}
+
+@end
