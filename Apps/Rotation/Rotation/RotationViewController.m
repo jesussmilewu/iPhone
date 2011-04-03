7ed78a83b9e12caa3882ef9fa8077ed843b61a03
Clemens Wagner <clemens@nostromo.local>
2011-04-03 18:51:11 +0200

    Speicherverwaltung

--- Apps/Rotation/Rotation/RotationViewController.m
+++ Apps/Rotation/Rotation/RotationViewController.m
@@ -2,12 +2,19 @@
 
 @implementation RotationViewController
 
+-(void)viewDidUnload {
+    [rotationControl release];
+    rotationControl = nil;
+    [super viewDidUnload];
+}
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)inInterfaceOrientation {
     return rotationControl.selectedSegmentIndex || inInterfaceOrientation == UIInterfaceOrientationPortrait;
 }
 
 - (void)dealloc {
     [rotationControl release];
+    rotationControl = nil;
     [super dealloc];
 }
 
