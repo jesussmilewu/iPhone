f0faa7f84590701a7bae73effe07494ce44491c5
Clemens Wagner <clemens@nostromo.local>
2011-05-22 21:58:26 +0200

    Unser Layer soll schöner werden

--- Apps/Layer/Layer/LayerViewController.m
+++ Apps/Layer/Layer/LayerViewController.m
@@ -8,6 +8,17 @@
 
 @end
 
+static float f(float x) {
+    float y = 0;
+    float a = 1.0;
+    
+    for(int i = 0; i < 20; ++i) {
+        y += sin(a * x);
+        a *= 2.0;
+    }
+    return 4.0 * y;
+}
+
 @implementation LayerViewController
 
 @synthesize scrollLayer;
@@ -17,12 +28,20 @@
     [super dealloc];
 }
 
+- (void)setUpLayer:(CALayer *)inLayer withFrame:(CGRect)inFrame {
+    inLayer.frame = inFrame;
+    inLayer.cornerRadius = 10;
+    inLayer.masksToBounds = YES;
+    inLayer.borderColor = [UIColor darkGrayColor].CGColor;
+    inLayer.borderWidth = 1.0;
+}
+
 - (CAGradientLayer *)gradientLayerWithFrame:(CGRect)inFrame {
     CAGradientLayer *theLayer = [[CAGradientLayer alloc] init];
     NSArray *theColors = [NSArray arrayWithObjects:(id)[UIColor redColor].CGColor, 
                           [UIColor greenColor].CGColor, [UIColor blueColor].CGColor, nil];
-    
-    theLayer.frame = inFrame;
+
+    [self setUpLayer:theLayer withFrame:inFrame];
     theLayer.colors = theColors;
     theLayer.startPoint = CGPointMake(0.0, 0.0);
     theLayer.endPoint = CGPointMake(1.0, 1.0);
@@ -34,7 +53,7 @@
     CGAffineTransform theIdentity = CGAffineTransformIdentity;
     CTFontRef theFont = CTFontCreateWithName((CFStringRef)@"Courier", 24.0, &theIdentity);
     
-    theLayer.frame = inFrame;
+    [self setUpLayer:theLayer withFrame:inFrame];
     theLayer.font = theFont;
     theLayer.fontSize = 20.0;
     theLayer.backgroundColor = [UIColor whiteColor].CGColor;
@@ -49,10 +68,31 @@
     CAScrollLayer *theLayer = [[CAScrollLayer alloc] init];
     CATextLayer *theTextLayer = [self textLayerWithFrame:CGRectMake(0.0, 0.0, CGRectGetWidth(inFrame), 4 * CGRectGetHeight(inFrame))];
         
-    theLayer.frame = inFrame;
+    [self setUpLayer:theLayer withFrame:inFrame];
     [theLayer addSublayer:theTextLayer];
     theTextLayer.fontSize *= 2;
     theLayer.scrollMode = kCAScrollVertically;
+    return [theLayer autorelease];
+}
+
+- (CAShapeLayer *)shapeLayerWithFrame:(CGRect)inFrame {
+    CAShapeLayer *theLayer = [[CAShapeLayer alloc] init];
+    CGMutablePathRef thePath = CGPathCreateMutable();
+    CGAffineTransform theIdentity = CGAffineTransformIdentity;
+    CGFloat theOffset = CGRectGetHeight(inFrame) / 2.0;
+    CGFloat theWidth = CGRectGetWidth(inFrame);
+
+    [self setUpLayer:theLayer withFrame:inFrame];
+    theLayer.backgroundColor = [UIColor whiteColor].CGColor;
+    theLayer.strokeColor = [UIColor blackColor].CGColor;
+    theLayer.fillColor = [UIColor clearColor].CGColor;
+    theLayer.lineWidth = 1.0;
+    CGPathMoveToPoint(thePath, &theIdentity, 0.0, f(0.0) + theOffset);
+    for(float x = 1.0; x < theWidth; x += 1.0) {
+        CGPathAddLineToPoint(thePath, &theIdentity, x, f(x * M_PI / theWidth) + theOffset);
+    }
+    theLayer.path = thePath;
+    CGPathRelease(thePath);
     return [theLayer autorelease];
 }
 
@@ -64,6 +104,7 @@
     [theLayer addSublayer:[self gradientLayerWithFrame:CGRectMake(10, 10, 300, 80)]];
     [theLayer addSublayer:[self textLayerWithFrame:CGRectMake(10, 100, 300, 80)]];
     [theLayer addSublayer:self.scrollLayer];
+    [theLayer addSublayer:[self shapeLayerWithFrame:CGRectMake(10, 310, 300, 80)]];
 }
 
 - (void)viewDidUnload {
