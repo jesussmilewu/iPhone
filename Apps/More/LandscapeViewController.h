bb5953e4d8e62a06cafb32b78cb7c699813377ba
Clemens Wagner <clemens@nostromo.local>
2011-06-12 15:59:30 +0200

    -

---
+++ Apps/More/LandscapeViewController.h
@@ -0,0 +1,16 @@
+//
+//  LandscapeViewController.h
+//  More
+//
+//  Created by Clemens Wagner on 12.06.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LandscapeViewController : UIViewController {
+    
+}
+
+@end
