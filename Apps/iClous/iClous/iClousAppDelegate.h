0331c218ea70611ab23e9632a620c69b4181b382
Klaus Rodewig <klaus@rodewig.de>
2011-07-22 22:34:20 +0200

    void

--- Apps/iClous/iClous/iClousAppDelegate.h
+++ Apps/iClous/iClous/iClousAppDelegate.h
@@ -1,16 +1,16 @@
 //
-//  iClousAppDelegate.h
-//  iClous
+//  iclousAppDelegate.h
+//  iclous
 //
-//  Created by Rodewig Klaus on 26.06.11.
-//  Copyright 2011 Klaus M. Rodewig. All rights reserved.
+//  Created by Klaus M. Rodewig on 02.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import "DeviceInfo.h"
 
-@interface iClousAppDelegate : NSObject <UIApplicationDelegate, CLLocationManagerDelegate>
+@interface iclousAppDelegate : UIResponder <UIApplicationDelegate>
 {
     CLLocationManager	*cllMgr;
     UITextView			*thisTextView;
