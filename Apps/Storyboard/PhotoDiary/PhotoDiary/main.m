0b261760ba33dd48732f13c1d290626adaeffd5d
Clemens Wagner <clemens@nostromo.local>
2012-04-01 20:00:15 +0200

    Umstellung auf Storyboard und ARC.

---
+++ Apps/Storyboard/PhotoDiary/PhotoDiary/main.m
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+#import "PhotoDiaryAppDelegate.h"
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([PhotoDiaryAppDelegate class]));
+        return retVal;
+    }
+}
