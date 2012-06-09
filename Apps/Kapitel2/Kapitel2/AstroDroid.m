9013a55b5412ec0518da331e82d5610f3be7935e
Klaus M. Rodewig <klaus@rodewig.de>
2012-06-09 22:36:31 +0200

    - Zwischenstand

---
+++ Apps/Kapitel2/Kapitel2/AstroDroid.m
@@ -0,0 +1,13 @@
+//
+//  AstroDroid.m
+//  Kapitel2
+//
+//  Created by Rodewig Klaus on 09.06.12.
+//  Copyright (c) 2012 Klaus M. Rodewig. All rights reserved.
+//
+
+#import "AstroDroid.h"
+
+@implementation AstroDroid
+
+@end
---
+++ Apps/Kapitel2/Kapitel2/ProtocolDroid.m
@@ -0,0 +1,13 @@
+//
+//  ProtocolDroid.m
+//  Kapitel2
+//
+//  Created by Rodewig Klaus on 09.06.12.
+//  Copyright (c) 2012 Klaus M. Rodewig. All rights reserved.
+//
+
+#import "ProtocolDroid.h"
+
+@implementation ProtocolDroid
+
+@end
