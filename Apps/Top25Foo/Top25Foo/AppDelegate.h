28fcd8cbca216c79aeb5b60d093cc67a0f6bc759
Klaus M. Rodewig <klaus@rodewig.de>
2012-08-02 21:47:04 +0200

    - Top25-Code-Beispiele

---
+++ Apps/Top25Foo/Top25Foo/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Top25Foo
+//
+//  Created by Klaus M. Rodewig on 01.08.12.
+//  Copyright (c) 2012 Klaus M. Rodewig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+@end
---
+++ Apps/Top25Foo/Top25Foo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Top25Foo
+//
+//  Created by Klaus M. Rodewig on 01.08.12.
+//  Copyright (c) 2012 Klaus M. Rodewig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
