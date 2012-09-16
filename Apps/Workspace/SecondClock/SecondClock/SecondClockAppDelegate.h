639bd435e05ffb119cb69708ada531bcf65cc27e
Clemens Wagner <clemens.wagner.2@gmx.de>
2012-09-16 11:26:08 +0200

    -

--- Apps/Workspace/SecondClock/SecondClock/SecondClockAppDelegate.h
+++ Apps/Workspace/SecondClock/SecondClock/SecondClockAppDelegate.h
@@ -10,6 +10,6 @@
 
 @interface SecondClockAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, strong) UIWindow *window;
 
 @end
