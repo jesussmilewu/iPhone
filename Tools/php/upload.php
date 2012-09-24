655a4d2f014dfff72d7a10e8c6911059b7a87084
Clemens Wagner <clemens.wagner.2@gmx.de>
2012-09-24 23:14:41 +0200

    no message

--- Tools/php/upload.php
+++ Tools/php/upload.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<title>Dateiupload</title>
 	</head>
 	<body>
 		<form enctype="multipart/form-data" method="POST">
-			Your name: <input type="text" name="name" /></br />
-			Choose a file to upload: <input name="file" type="file" /><br />
+			Code: <input type="text" name="code" /></br />
+			Message: <input type="text" name="message" /></br />
+			Choose a photo to upload: <input name="photo" type="file" /><br />
 			<input type="submit" value="Upload File" />
 		</form>
+		<?php
+			if(isset($_FILES['photo']) && move_uploaded_file($_FILES['photo']['tmp_name'], "uploads/".$_FILES['photo']['name'])) {
+				echo "Moved <emph>".$_FILES['photo']['tmp_name']."</emph> to <emph>".$_FILES['photo']['name']."</emph> for <strong>".$_POST['code'].".</strong><br/>";
+			}
+		?>
 	</body>
 </html>
-<?php
-	if(isset($_FILES['file']) && move_uploaded_file($_FILES['file']['tmp_name'], "uploads/".$_FILES['file']['name'])) {
-		echo "Moved <emph>".$_FILES['file']['tmp_name']."</emph> to <emph>".$_FILES['file']['name']."</emph> for <strong>".$_POST['name'].".</strong><br/>";
-	}
-?>
