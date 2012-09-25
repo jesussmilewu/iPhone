6c221ac85d0910e9b2dfdfbef2e39b998bfcd046
Clemens Wagner <clemens.wagner.2@gmx.de>
2012-09-25 23:24:22 +0200

    POST-Anfragen

--- Tools/php/upload.php
+++ Tools/php/upload.php
@@ -6,14 +6,15 @@
 	</head>
 	<body>
 		<form enctype="multipart/form-data" method="POST">
-			Code: <input type="text" name="code" /></br />
-			Message: <input type="text" name="message" /></br />
+			Code: <input type="text" name="code" value="<?php echo $_POST['code'] ?>"/></br />
+			Message: <input type="text" name="message" value="<?php echo $_POST['message'] ?>" /></br />
 			Choose a photo to upload: <input name="photo" type="file" /><br />
 			<input type="submit" value="Upload File" />
 		</form>
 		<?php
 			if(isset($_FILES['photo']) && move_uploaded_file($_FILES['photo']['tmp_name'], "uploads/".$_FILES['photo']['name'])) {
-				echo "Moved <emph>".$_FILES['photo']['tmp_name']."</emph> to <emph>".$_FILES['photo']['name']."</emph> for <strong>".$_POST['code'].".</strong><br/>";
+				echo "<p class='result'>Moved <emph>".$_FILES['photo']['tmp_name']."</emph> to <emph>".$_FILES['photo']['name'].
+				"</emph> for <strong>".$_POST['code'].".</strong></p>";
 			}
 		?>
 	</body>
