4669115822bfd634f07a100f2255600e98373bc6
Clemens Wagner <clemens.wagner.2@gmx.de>
2013-04-10 23:01:43 +0200

    no message

--- Apps/Storyboard/WebView/WebView/AppDelegate.m
+++ Apps/Storyboard/WebView/WebView/AppDelegate.m
@@ -12,10 +12,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-    splitViewController.delegate = (id)navigationController.topViewController;
+    UISplitViewController *theSplitViewController = (UISplitViewController *)self.window.rootViewController;
+    UINavigationController *navigationController = [theSplitViewController.viewControllers lastObject];
+    
+    theSplitViewController.delegate = (id)navigationController.topViewController;
     return YES;
 }
 							
