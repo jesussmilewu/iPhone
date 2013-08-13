7e590549309595c4f34fec2b786177e3f1f70d5a
Clemens Wagner <clemens.wagner.2@gmx.de>
2013-08-13 22:34:25 +0200

    Modal für iOS 6 / 7

--- Apps/iOS6/Modal/Modal/main.m
+++ Apps/iOS6/Modal/Modal/main.m
@@ -2,15 +2,15 @@
 //  main.m
 //  Modal
 //
-//  Created by Clemens Wagner on 23.01.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Clemens Wagner on 12.08.13.
+//  Copyright (c) 2013 Cocoaneheads. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 #import "ModalAppDelegate.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([ModalAppDelegate class]));
---
+++ Apps/iOS6/StoryboardAlarmClock/AlarmClock/AlarmClockAppDelegate.m
@@ -0,0 +1,71 @@
+//
+//  AlarmClockAppDelegate.m
+//  AlarmClock
+//
+//  Created by Clemens Wagner on 17.07.13.
+//  Copyright (c) 2013 Cocoaneheads. All rights reserved.
+//
+
+#import "AlarmClockAppDelegate.h"
+#import <AudioToolbox/AudioToolbox.h>
+
+@interface AlarmClockAppDelegate()
+
+@property (nonatomic, strong) NSNumber *soundId;
+
+@end
+
+@implementation AlarmClockAppDelegate
+
+@synthesize soundId = _soundId;
+
+- (void)dealloc {
+    self.soundId = nil;
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+							
+- (void)application:(UIApplication *)inApplication didReceiveLocalNotification:(UILocalNotification *)inNotification {
+    if(inApplication.applicationState == UIApplicationStateActive) {
+        UIAlertView *theAlert = [[UIAlertView alloc] initWithTitle:nil
+                                                           message:inNotification.alertBody
+                                                          delegate:nil
+                                                 cancelButtonTitle:NSLocalizedString(@"OK", @"OK")
+                                                 otherButtonTitles:nil];
+        [theAlert show];
+        [self playSound];
+    }
+}
+
+- (NSNumber *)soundId {
+    if(_soundId == nil) {
+        NSURL *theURL = [[NSBundle mainBundle] URLForResource:@"ringtone" withExtension:@"caf"];
+        SystemSoundID theId;
+
+        if(AudioServicesCreateSystemSoundID((__bridge CFURLRef)theURL, &theId) == kAudioServicesNoError) {
+            self.soundId = @(theId);
+        }
+    }
+    return _soundId;
+}
+
+- (void)setSoundId:(NSNumber *)inSoundId {
+    if(_soundId != inSoundId) {
+        if(_soundId != nil) {
+            AudioServicesDisposeSystemSoundID([_soundId unsignedIntValue]);
+        }
+        _soundId = inSoundId;
+    }
+}
+
+- (void)playSound {
+    NSNumber *theId = self.soundId;
+
+    if(theId) {
+        AudioServicesPlaySystemSound([theId unsignedIntValue]);
+    }
+}
+
+@end
