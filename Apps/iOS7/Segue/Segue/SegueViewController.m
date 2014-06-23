8a47631a6228c8ed9d4bd6d483d5a3bc903f131a
Clemens Wagner <clemens.wagner.2@gmx.de>
2014-06-23 20:47:38 +0200

    Runtime Attribute

--- Apps/iOS7/Segue/Segue/SegueViewController.m
+++ Apps/iOS7/Segue/Segue/SegueViewController.m
@@ -8,14 +8,21 @@
 
 #import "SegueViewController.h"
 #import "ResultViewController.h"
+#import "Cube.h"
 
 @interface SegueViewController ()
 
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (strong, nonatomic) IBOutlet Cube *cube;
 
 @end
 
 @implementation SegueViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Cube: length = %.f, color = %@", self.cube.length, self.cube.color);
+}
 
 - (void)prepareForSegue:(UIStoryboardSegue *)inSegue sender:(id)inSender {
     if([inSegue.identifier isEqualToString:@"dialog"]) {
